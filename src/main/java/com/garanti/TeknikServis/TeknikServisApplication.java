88bd7a48e6176bb3020e16bd47c724e1c01e5d65
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2023-02-09 10:46:33 +0300

    first commit

---
+++ src/main/java/com/garanti/TeknikServis/TeknikServisApplication.java
@@ -0,0 +1,15 @@
+package com.garanti.TeknikServis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+public class TeknikServisApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(TeknikServisApplication.class, args);
+	}
+}
+
