c91da72d482e958824c2490e86b0e66a6ebe5d48
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2023-02-09 10:50:24 +0300

    spring security was included in the project

---
+++ src/main/java/com/garanti/TeknikServis/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.garanti.TeknikServis.config;
+
+import com.garanti.TeknikServis.security.JWTAuthenticationFilter;
+import com.garanti.TeknikServis.security.JWTAuthorizationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@Profile("default")
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.csrf().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilter(new JWTAuthenticationFilter(authenticationManager()));
+        http.addFilterAfter(new JWTAuthorizationFilter(), JWTAuthenticationFilter.class);
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
